c66170e9e041562eec176f3bf1577ad32ae67cd0
TamiloreA <tamiloreakinsola@gmail.com>
2023-09-10 23:04:18 +0100

    register feature

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=120)
+    phone = models.CharField(max_length=300)
+    password = models.CharField(max_length=400)
+
+
+
+    def __str__(self):
+        return f"{self.name}  { self.password } "
+
+
+def image(instance, filename):
+    return 'uploads/{}/{}'.format(instance.title, filename)
+
+
+'''The Post model '''
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField(max_length=400)
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import Author
+from django.db.models import Q
 # Create your views here.
 def index(request):
     # return HttpResponse('<h1>Welcome to our pyhton class</h1>')
@@ -11,8 +11,24 @@ def about(request):
     # return HttpResponse('<h1>This is our about us page</h1>')
     return render(request, 'destiny.html')
 def contact(request):
+    if request.method == "POST" : 
+        print("form triggered")
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        password = request.POST['password']
+        
+        check_author = Author.objects.filter(Q(name__icontains=name) | Q(email__icontains=email)).exists()
+        if check_author : 
+            print("this user exists already")
+        else :
+            new_author = Author(name=name, email=email, phone=phone, password=password)
+            new_author.save()
+            
+            print("New user added successfully")
+            
     # return HttpResponse('<h1>This is our contact page</h1>')
-     return render(request, 'registration.html')
+    return render(request, 'registration.html')
  
 def demo(request) : 
     pass 
