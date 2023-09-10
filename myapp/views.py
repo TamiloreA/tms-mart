0ee72fee3c0760fa66c66b224142925ceab6f335
TamiloreA <tamiloreakinsola@gmail.com>
2023-09-10 11:13:56 +0100

    initial

---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('<h1>Welcome to our pyhton class</h1>')
+    return render(request, 'hello.html')
+
+def about(request):
+    # return HttpResponse('<h1>This is our about us page</h1>')
+    return render(request, 'destiny.html')
+def contact(request):
+    # return HttpResponse('<h1>This is our contact page</h1>')
+     return render(request, 'registration.html')
